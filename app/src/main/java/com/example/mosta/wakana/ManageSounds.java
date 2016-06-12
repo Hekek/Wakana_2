05bf877de4ddbf471366c0712339badab67e0997
Hekek <f4ad94@gmail.com>
2016-06-12 15:00:53 +0200

    Added onResume to update list

--- app/src/main/java/com/example/mosta/wakana/ManageSounds.java
+++ app/src/main/java/com/example/mosta/wakana/ManageSounds.java
@@ -1,5 +1,6 @@
 package com.example.mosta.wakana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,13 +37,32 @@ public class ManageSounds extends AppCompatActivity {
         soundsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String info = ((TextView) view).getText().toString();
-                toast(info);
-
+                String label = ((TextView) view).getText().toString();
+                Intent i = new Intent(getApplicationContext(),ManageSound.class).putExtra("LABEL",label);
+                startActivity(i);
             }
         });
     }
     public void toast(String text){
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        ListView soundsList = (ListView) findViewById(R.id.sounds_list);
+        DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+        listLabels = db.getListLabels();
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.sounds_list_component, listLabels);
+        ListView listView = (ListView) findViewById(R.id.sounds_list);
+        listView.setAdapter(adapter);
+        soundsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String label = ((TextView) view).getText().toString();
+                Intent i = new Intent(getApplicationContext(),ManageSound.class).putExtra("LABEL",label);
+                startActivity(i);
+            }
+        });
     }
 }
